a5c01bd2947e7deadafc4fc9a3c5dde4a26eb93a
Jamie2345 <jamiecliffe@icloud.com>
2023-04-07 11:13:52 +0100

    Added an unfriend user DELETE route to api

--- server/controllers/FriendController.js
+++ server/controllers/FriendController.js
@@ -109,15 +109,15 @@ const accept = (req, res, next) => {
         name: currentUser.username
       })
 
-      // remove all the the pending requests now accepted
-      userFound.pendingFriends.outgoing = userFound.pendingFriends.outgoing.filter(obj => obj.name !== currentUser.username);
+      // remove all the the pending requests as they are now accepted
+      userFound.pendingFriends.outgoing = userFound.pendingFriends.outgoing.filter(pending => pending.name !== currentUser.username);
 
       currentUser.friends.push({
         userId: userFound._id,
         name: userFound.username
       });
 
-      currentUser.pendingFriends.incoming = currentUser.pendingFriends.incoming.filter(obj => obj.name !== userFound.username);
+      currentUser.pendingFriends.incoming = currentUser.pendingFriends.incoming.filter(pending => pending.name !== userFound.username);
 
       // save changes to database
       Promise.all([userFound.save(), currentUser.save()])
@@ -148,15 +148,74 @@ const accept = (req, res, next) => {
   
 }
 
+const unfriend = (req, res, next) => {
+  
+  const {unfriendUser} = req.body;
+
+  User.findById(req.userInfo.id) // req.userInfo.id is the id of the user making the request (.userInfo is created in a middleware function)
+  .then(currentUser => {
+
+    // check if currentUser is found
+    if (!currentUser) {
+      res.status(404).json({ message: 'Invalid user ID' });
+      return;
+    }
+
+    // if not already friends
+    if (!(currentUser.friends.some(friend => friend.name === unfriendUser))) {
+      res.status(409).json({ message: 'This user is not your friend' });
+      return;
+    }
+
+    // find the user to accept
+    User.findOne({username: unfriendUser})
+    .then(userFound => {
+
+      if (!userFound) {
+        res.status(404).json({ message: 'User does not exist' });
+        return;
+      }
+
+      // remove the user from the friends list (filter them out)
+      userFound.friends = userFound.friends.filter(friend => friend.name !== currentUser.username);
+      // remove the friend from the other person as well
+      currentUser.friends = currentUser.friends.filter(friend => friend.name !== userFound.username); // userFound.username === unfriendUser Variable
+
+      // save changes to database
+      Promise.all([userFound.save(), currentUser.save()])
+      .then(() => {
+        res.json({
+          message: 'success you have successfully unfriended the user'
+        });
+      })
+      .catch(err => {
+        res.json({
+          message: 'unexpected error unfriending the user'
+        });
+      });
+    })
+
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(404)
+    });
+
+  });
+}
+
+
 module.exports = {
   // creating decks and flashcards and editing and deleting cards
   request,
-  accept
+  accept,
+
+  unfriend
 
 
-  // request TYPE = POST
-  // accept TYPE = POST
-  // unfriend TYPE = POST
+  // request TYPE = PUT
+  // accept TYPE = PUT
+  // unfriend TYPE = DELETE
+  // unfriend TYPE = DELETE
 
   // incoming TYPE = GET
   // outgoing TYPE = GET
--- server/routes/friends.js
+++ server/routes/friends.js
@@ -5,5 +5,6 @@ const FriendController = require('../controllers/FriendController')
 
 router.put('/request', FriendController.request)
 router.put('/accept', FriendController.accept)
+router.delete('/unfriend', FriendController.unfriend)
 
 module.exports = router
